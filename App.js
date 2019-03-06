6e98222ebaecf593e2fed3cb71f556f170aca13f
Mike <szekleym@gmail.com>
2019-03-06 13:59:33 -0700

    blocked due to environment differences, solution to deploy backend to firebase-- saving changes

--- App.js
+++ App.js
@@ -4,13 +4,38 @@ import CreateBlog from './components/createblog.js'
 import Dashboard from './components/dashboard.js'
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state =
-    { text: "useless placeholder" }
+    this.state = {
+      blogs: []
+    }
   }
 
+  // componentDidMount() {
+  //   this.getData()
+  // }
 
+  // getData = async() => {
+  //   const url = 'http://localhost:3002'
+  //   try {
+  //     const info = await fetch(url)
+  //     const data = await info.json()
+  //     console.log(data)
+  //   } catch(err) {
+  //     console.log(err)
+  //   }
+  // }
+
+  // async componentDidMount() {
+  //   const url = 'http://127.0.0.1:3002/test'
+  //   try { 
+  //     const api = await fetch(url)
+  //     const awaitapi = await api.json()
+  //     this.setState({blogs: awaitapi})
+  //   } catch(err) {
+  //     console.log("Error fetching data-----------", err)
+  //   }
+  // }
 
   change = (text) => {
     console.log("text: ", text.nativeEvent.text)
