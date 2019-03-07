dc6e4d4e8601e04109a1d1692bb8f045e7100109
powerbarrusa <barr@covrigaru.com>
2019-03-07 11:48:35 -0700

    works

--- components/trending.js
+++ components/trending.js
@@ -1,23 +1,20 @@
 import React from 'react'
-import { FlatList, View, Text, Button } from 'react-native'
+import { ScrollView, View, Text, Button } from 'react-native'
 
 const Trending = (props) => {
-  console.log('trending props', props)
   return (
-    <View>
-      <Text style={{
-        backgroundColor: 'grey',
-        fontWeight: "bold",
-        marginTop: 50
-        }}>Trending posts</Text>
+    <ScrollView>
       <View style={{
         flexDirection: 'column',
         alignItems: 'center',
-        marginTop: 50
+        marginTop: 100
       }}>
-      <View>{props.posts.map(post => <Button color="#841584" title={post.title} key={post.id}></Button>)}</View>
+        {/* <Tooltip popover={<Text>Info here</Text>}>
+          <Text>Press me</Text>
+        </Tooltip> */}
+        <View>{props.posts.map(post => <Button title={post.title} key={post.id}></Button>)}</View>
       </View>
-    </View>
+    </ScrollView>
   )
 }
 
